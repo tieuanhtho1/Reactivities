6934439ac7292042dd9c512b7b34396fd8af9a60
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2023-06-12 20:03:55 +0700

    add constant for api link, test react call api by axios

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { API } from './utilities/constants';
 
 function App() {
+  const [activities, setActivities] = useState<any>(['',''])
+  useEffect(() => {
+    axios.get(`${API}/activities`)
+    .then(response => {
+      setActivities(response.data)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {activities.forEach((activity : any) => {
+          <li>
+            {activity}
+          </li>
+        })}
       </header>
     </div>
   );
