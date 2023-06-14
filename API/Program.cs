92181683dfcf9ad6e25c8eed30771cfd14fe5506
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2023-06-14 16:37:34 +0700

    add auto mapper, delete activity

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using Application.Activities;
+using Application.Core;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -26,6 +27,7 @@ builder.Services.AddCors(opt =>
 });
 
 builder.Services.AddMediatR(typeof(List.Handler));
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 var app = builder.Build();
 
