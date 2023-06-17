e3fe8e4a99c74611c9e9d32e9680cef12222c145
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2023-06-17 13:35:34 +0700

    make program.cs thinner

--- API/Program.cs
+++ API/Program.cs
@@ -1,33 +1,12 @@
 
-using Application.Activities;
-using Application.Core;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+using API.Extensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(options => 
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt => 
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
