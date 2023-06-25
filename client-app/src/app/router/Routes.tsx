47a3d398e199490c111c96a927de9763009eb18f
tieuanhtho1 <52588394+tieuanhtho1@users.noreply.github.com>
2023-06-25 16:53:08 +0700

    undone

---
+++ client-app/src/app/router/Routes.tsx
@@ -0,0 +1,10 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+
+export const routes: RouteObject[] = [
+    {
+        path: "/",
+
+    }
+]
+
+export const router = createBrowserRouter()
